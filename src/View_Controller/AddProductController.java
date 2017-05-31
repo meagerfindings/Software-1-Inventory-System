d68a259f86097b3cf5469e0438ddba6f5e44dddc
Mat Greten <mgreten@gmail.com>
2017-05-31 16:17:44 -0600

    Spelling Error
    Changed thant to than in price error dialog.

--- src/View_Controller/AddProductController.java
+++ src/View_Controller/AddProductController.java
@@ -461,7 +461,7 @@ public class AddProductController implements Initializable {
                 Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.setTitle("Product Price Error");
                 alert.setHeaderText("Product Price is to low.");
-                alert.setContentText("The price of a product cannot be lower thant the price of its parts.\n\nCost of Parts: $" + partsTotalPrice + "\nProduct price entered: $" + priceCost);
+                alert.setContentText("The price of a product cannot be lower than the price of its parts.\n\nCost of Parts: $" + partsTotalPrice + "\nProduct price entered: $" + priceCost);
 
                 alert.showAndWait();
 
